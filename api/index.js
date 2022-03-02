adbc83e055e3f1fb653edce03b09b22347c65086
Zach Coley <zachcoley@users.noreply.github.com>
2022-03-03 10:11:49 +1300

    Created a basic node express web server that responds with a hello world response

---
+++ api/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+const port = 3001;
+
+app.get('/', (req, res) => {
+  res.end('Hello World');
+});
+
+app.listen(port, () => console.log(`The server is listening on port ${port}`));
