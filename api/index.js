e179aebab2c86831b5fb37f2aa8e923bd2aeecf9
Zach Coley <zachcoley@users.noreply.github.com>
2022-03-03 11:51:33 +1300

    Imported bootstrap using sass and customized the primary color, modified the server to respond with the react build index.html page and created a simple react page to test my modified bootstrap was working

--- api/index.js
+++ api/index.js
@@ -1,10 +1,16 @@
+// Bring in all dependencies
 const express = require('express');
+const path =  require('path');
 
+// Create a instance of express.js
 const app = express();
+
+// Set the port to 3001
 const port = 3001;
 
-app.get('/', (req, res) => {
-  res.end('Hello World');
+// All GET requests that we do not handle will return the index.html page of the react app
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../scp-client-app/build', 'index.html'));
 });
 
 app.listen(port, () => console.log(`The server is listening on port ${port}`));
---
+++ scp-client-app/src/App.jsx
@@ -0,0 +1,17 @@
+import {
+  Button,
+  Container
+} from 'react-bootstrap';
+
+const App = () => {
+  return (
+    <>
+      <Container>
+        <h1>Hello World</h1>
+        <Button variant='primary'>Click me</Button>
+      </Container>
+    </>
+  );
+}
+
+export default App;
